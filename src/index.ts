109e9393ec4cb5624b4a251c6ef7e82b14faa5f7
DenisTimofijuk <denis.timofijuk@gmail.com>
2021-01-11 19:02:10 +0200

    pixels implemented. Drawing implemented.

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,76 @@
+import Pixel from "./pixel";
+
+const pixels:Pixel[] = [];
+
+function initPixels() {
+  const screenCanvas = document.getElementById("eye") as HTMLCanvasElement;
+  const TOTAL_PIXELS = 25;
+  const devider = Math.sqrt(TOTAL_PIXELS);
+  const pixelWidth = screenCanvas.width / devider;
+  const pixelHeight = screenCanvas.height / devider;
+  let x = 0;
+  let y = 0;
+  for (let index = 0; index < TOTAL_PIXELS; index++) {  
+    pixels.push(new Pixel(pixelWidth, pixelHeight, x, y, screenCanvas, index));
+    x += pixelWidth;
+    if(x >= screenCanvas.width){
+      y += pixelHeight;
+      x = 0;
+    }    
+  }
+
+  return function draw(event:MouseEvent){
+    if(event.buttons !== 1 && event.buttons !== 2 ){
+      return;
+    }
+  
+    const x = Math.ceil(event.offsetX / pixelWidth);
+    const y = Math.ceil(event.offsetY / pixelHeight);
+    const index = y * devider - devider + x - 1;
+
+    if(event.buttons === 1){
+      pixels[index] && pixels[index].draw();
+    }
+    if(event.buttons === 2){
+      pixels[index] && pixels[index].clear();
+    }
+  }
+}
+
+function indicatePixels() {
+  for(let pixel of pixels){
+    pixel.debug();
+  }
+}
+
+function initScreenEvents() {
+  const screenCanvas = document.getElementById("eye") as HTMLCanvasElement;
+  screenCanvas.addEventListener("mousemove", draw);
+  screenCanvas.addEventListener("contextmenu",  ( e )=> { e.preventDefault(); return false; } );
+}
+
+const draw = initPixels();
+indicatePixels();
+initScreenEvents();
+
+
 /**
- * This file is just a silly example to show everything working in the browser.
- * When you're ready to start on your site, clear the file. Happy hacking!
- **/
-
-import confetti from 'canvas-confetti';
-
-confetti.create(document.getElementById('canvas') as HTMLCanvasElement, {
-  resize: true,
-  useWorker: true,
-})({ particleCount: 200, spread: 200 });
+ * 25 pixels
+ * 
+ * class Pixel{}. It should 
+ * 
+ * class Neuron{
+ *  state = ON | OFF
+ * } 
+ * 
+ * class Layer{
+ *  name = RETINA | V1 | V2 | V4 | IT
+ *  neurons = [new Neuron()]
+ * }
+ * 
+ * IT layer - 10 neurons each refering to number 0-9
+ * 
+ * 
+ *  
+ * 
+ */
---
+++ src/pixel.ts
@@ -0,0 +1,69 @@
+export default class Pixel {
+    width: number;
+    height: number;
+    canvas: HTMLCanvasElement;
+    ctx: CanvasRenderingContext2D;
+    x: number;
+    y: number;
+    screen: HTMLCanvasElement;
+    index: number;
+    active: boolean;
+    constructor(width: number, height: number, x: number, y: number, screen: HTMLCanvasElement, index:number) {
+      this.active = false;
+      this.index = index;
+      this.screen = screen;
+      this.x = x;
+      this.y = y;
+      this.width = width;
+      this.height = height;
+      this.canvas = document.createElement('canvas');
+      this.canvas.width = width;
+      this.canvas.height = height;
+      this.ctx = this.canvas.getContext('2d') as CanvasRenderingContext2D;
+      this.init();
+      this.display();
+    }
+  
+    init() {
+      this.ctx.beginPath();
+      this.ctx.strokeStyle = "black";
+      this.ctx.lineWidth = 1;
+      this.ctx.strokeRect(0, 0, this.width, this.height);
+      this.ctx.closePath();
+    }
+  
+    draw() {
+        if(this.active){
+            return;
+        }
+        const padding = 5;
+        this.active = true;
+        this.ctx.beginPath();
+        this.ctx.fillStyle = "purple";
+        this.ctx.fillRect(padding, padding, this.width - padding*2, this.height - padding*2);
+        this.ctx.closePath();
+
+        this.display();
+    }
+  
+    display(){
+      const screenContext = this.screen.getContext('2d');
+      screenContext?.clearRect(this.x, this.y, this.width, this.height);
+      screenContext?.drawImage(this.canvas, this.x, this.y);
+    }
+  
+    clear() {
+        this.active = false;
+        this.ctx.clearRect(0,0, this.width, this.height);
+        this.init();
+        this.debug();
+        this.display();
+    }
+
+    debug(){
+        this.ctx.fillStyle = "black";
+        this.ctx.textAlign = "center";
+        this.ctx.fillText(this.index.toString(), this.width / 2, this.height / 2);
+        this.display();
+    }
+  }
