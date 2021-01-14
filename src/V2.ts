ad678514a8ddb0f01c1efc1227048eb84fd56321
DenisTimofijuk <denis.timofijuk@gmail.com>
2021-01-14 20:14:40 +0200

    V2 right-up neuron group added.

--- src/V2.ts
+++ src/V2.ts
@@ -5,17 +5,18 @@ import { referenceCount, V1neuronsInLine, verticalNeuronsFirstIndex } from "./V1
 
 export function initV2Layer(V1: Layer, V2: Layer) {
     let verticalNeuronIndex = verticalNeuronsFirstIndex;
-    right_down();
+    left_down();
     right_up();
+    right_down();
 
-    function right_down() {
+    function left_down() {
         let reference_index_1 = 0;
         let reference_index_2 = verticalNeuronIndex;
         let lastAvailableIndex = (PIXEL_STEP - 2) * V1neuronsInLine;
         while (reference_index_1 < lastAvailableIndex) {
             for (let i = 0; i < V1neuronsInLine; i++) {
                 let neuron = new Neuron();
-                neuron.name = "angle-right-down";
+                neuron.name = "angle-left-down";
                 neuron.references.push(
                     V1.neurons[reference_index_1],
                     V1.neurons[reference_index_2]
@@ -51,6 +52,32 @@ export function initV2Layer(V1: Layer, V2: Layer) {
             }else{
                 reference_index_2 += V1neuronsInLine;
             }
+        }
+    }
+
+    function right_down() {
+        let firstVerticalCompatibleIndex = verticalNeuronsFirstIndex + (referenceCount - 1) * V1neuronsInLine;
+        let lastHorizontalCompatibleIndex = verticalNeuronsFirstIndex - (referenceCount - 1) * V1neuronsInLine - 1;
+        let counter = V1neuronsInLine;
+        let reference_index_2 = firstVerticalCompatibleIndex;
+        for(let reference_index_1 = 0; reference_index_1 <= lastHorizontalCompatibleIndex; reference_index_1++){
+            let neuron = new Neuron();
+            neuron.name = "angle-right-down";
+            neuron.references.push(
+                V1.neurons[reference_index_1],
+                V1.neurons[reference_index_2]
+            );
+            V2.neurons.push(neuron);
+            
+            --counter;
+            if(counter <= 0){
+                counter = V1neuronsInLine;
+                firstVerticalCompatibleIndex++;
+                reference_index_2 = firstVerticalCompatibleIndex;
+            }else{
+                reference_index_2 += V1neuronsInLine;
+            }
+
         }
     }
 
