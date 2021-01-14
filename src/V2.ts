fe32f50bd13189b0dc27b027c1a80eae1669b1ba
DenisTimofijuk <denis.timofijuk@gmail.com>
2021-01-14 20:24:38 +0200

    V2 left-up neuron group added.

--- src/V2.ts
+++ src/V2.ts
@@ -5,6 +5,7 @@ import { referenceCount, V1neuronsInLine, verticalNeuronsFirstIndex } from "./V1
 
 export function initV2Layer(V1: Layer, V2: Layer) {
     let verticalNeuronIndex = verticalNeuronsFirstIndex;
+    left_up();
     left_down();
     right_up();
     right_down();
@@ -78,6 +79,31 @@ export function initV2Layer(V1: Layer, V2: Layer) {
                 reference_index_2 += V1neuronsInLine;
             }
 
+        }
+    }
+
+    function left_up() {
+        let firstHorizontalCompatibleIndex = (referenceCount - 1) * V1neuronsInLine;
+        let firstVerticalCompatibleIndex = verticalNeuronsFirstIndex;
+        let counter = V1neuronsInLine;
+        let reference_index_2 = verticalNeuronsFirstIndex;
+        for(let reference_index_1 = firstHorizontalCompatibleIndex; reference_index_1 < verticalNeuronsFirstIndex; reference_index_1++){
+            let neuron = new Neuron();
+            neuron.name = "angle-left-up";
+            neuron.references.push(
+                V1.neurons[reference_index_1],
+                V1.neurons[reference_index_2]
+            );
+            V2.neurons.push(neuron);
+            
+            --counter;
+            if(counter <= 0){
+                counter = V1neuronsInLine;
+                firstVerticalCompatibleIndex++;
+                reference_index_2 = firstVerticalCompatibleIndex;
+            }else{
+                reference_index_2 += V1neuronsInLine;
+            }
         }
     }
 
